a0657afc6c0e0d430688928a86a0c830ccb1c856
Jeerhz <adlesalem@hotmail.fr>
2025-01-08 22:09:44 +0100

    fix: add newline at end of file in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel';
 import { defineConfig } from 'astro/config';
 
+// To deploy
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://adle-portfolio.vercel.app/', // Required for sitemap -> Replace with your site's URL
